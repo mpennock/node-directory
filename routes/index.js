d28e907de0b559fd0906c5f0e70fd1471821240f
mpennock <pennockmartin@gmail.com>
2016-03-21 14:57:24 -0400

    added directory page and rout

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// get directory page
+router.get('/directory', function(req, res, next) {
+	// show the directory view
+	res.render('directory');
+});
+
 module.exports = router;
